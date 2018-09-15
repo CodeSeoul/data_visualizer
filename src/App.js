6468db0ba09d24298fb6576c3c87c5e579858222
joelquek <joel.qcx@gmail.com>
2018-09-15 10:28:07 +0900

    setup tab menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ChartContainer from './components/ChartContainer';
 import UploadButton from './components/UploadButton';
+import TabMenu from './components/TabMenu';
 
 const App = () => {
     return (
       <div className="App">
           <UploadButton />
+          <TabMenu />
           <ChartContainer />
       </div>
     );
---
+++ src/components/TabMenu.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const TabMenu = () => {
+    return (
+    <nav>
+        <ul>
+            <li><a href="#">Table</a></li>
+            <li><a href="#bar">Bar</a></li>
+            <li><a href="#line">Line</a></li>
+            <li><a href="#pie">Pie</a></li>
+            <li><a href="#scatter">Scatter</a></li>
+        </ul>
+    </nav>
+    );
+};
+
+export default TabMenu;
