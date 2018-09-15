3a8b9ef7856b5a2aeb87f584e3a7349c245bb3e4
joelquek <joel.qcx@gmail.com>
2018-09-15 09:56:12 +0900

    clean project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import ChartContainer from './components/ChartContainer'
 
 const App = () => {
     return (
---
+++ src/components/ChartContainer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ChartContainer = () =>{
+    <div> Chart Container </div>
+}
+
+export {ChartContainer};
