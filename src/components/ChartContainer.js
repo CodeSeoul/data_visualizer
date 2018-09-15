45c462b585375fe5d8d7e659158e7c0af912338c
joelquek <joel.qcx@gmail.com>
2018-09-15 10:17:28 +0900

    modified chartcontainer style

--- src/components/ChartContainer.js
+++ src/components/ChartContainer.js
@@ -3,13 +3,13 @@ import React from 'react';
 const chartContainerStyle = {
     textAlign:'center',
     border: '1px solid black',
-    padding: 50
-}
+    padding: 50,
+};
 
 const ChartContainer = () =>{
     return  (
         <div style={chartContainerStyle}> Chart Container </div>
     )
-}
+};
 
 export default ChartContainer;
