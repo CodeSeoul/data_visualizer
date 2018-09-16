2d16aa3923b51c83266beb3e37afe505196957bd
joelquek <joel.qcx@gmail.com>
2018-09-16 11:06:49 +0900

    setup tabmenu

--- src/components/TabMenu.js
+++ src/components/TabMenu.js
@@ -1,16 +1,14 @@
 import React from 'react'
 
-const TabMenu = () => {
+const TabMenu = (toggleChartHandler) =>{
     return (
-    <nav>
-        <ul>
-            <li><a href="#">Table</a></li>
-            <li><a href="#bar">Bar</a></li>
-            <li><a href="#line">Line</a></li>
-            <li><a href="#pie">Pie</a></li>
-            <li><a href="#scatter">Scatter</a></li>
-        </ul>
-    </nav>
+        <div>
+            <input onClick={toggleChartHandler} value='Table' type='button'/>
+            <input value='Bar' type='button'/>
+            <input value='Line' type='button'/>
+            <input value='Pie' type='button'/>
+            <input value='Scatter' type='button'/>
+        </div>
     );
 };
 
