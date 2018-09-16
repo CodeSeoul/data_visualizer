b5c539a06faa43da673966e2a4780e22691a6367
joelquek <joel.qcx@gmail.com>
2018-09-16 11:06:03 +0900

    setup chartcontainer

--- src/components/ChartContainer.js
+++ src/components/ChartContainer.js
@@ -1,15 +1,35 @@
 import React from 'react';
+import TabMenu from '../components/TabMenu';
+import BarChart from '../components/charts/BarChartComponent';
+import LineChart from '../components/charts/LineChartComponent';
+import PieChart from '../components/charts/PieChartComponent';
+import ScatterPlot from '../components/charts/ScatterPlotComponent';
 
 const chartContainerStyle = {
     textAlign:'center',
     border: '1px solid black',
     padding: 50,
 };
+class ChartContainer extends React.Component{
+    state = {
+        selectedChart : 'table'
+    }
 
-const ChartContainer = () =>{
-    return  (
-        <div style={chartContainerStyle}> Chart Container </div>
-    )
+    toggleChartHandler = chartType => {
+        this.setState({
+            selectedTab: chartType
+        });
+    };
+    render(){
+        return  (
+            <div>
+                <TabMenu selectedChart={this.state.selectedChart}/>
+                <div style={chartContainerStyle}> 
+                    Chart Container
+                </div>
+            </div>
+        )
+    };
 };
 
 export default ChartContainer;
