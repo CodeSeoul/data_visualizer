6e357e937ad289f244182e55012adec9d328d395
Joel Quek <joel.qcx@gmail.com>
2018-09-16 15:10:12 +0900

    removed tabmenu from App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ChartContainer from './components/ChartContainer';
 import UploadButton from './components/UploadButton';
-import TabMenu from './components/TabMenu';
 
 const App = () => {
     return (
