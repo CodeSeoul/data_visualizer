65461f231203bb77417b5a257f77826dc241310c
Joel Quek <joel.qcx@gmail.com>
2018-09-27 22:32:54 +0900

    add columnlist component into piechart

--- src/components/charts/PieChartComponent.js
+++ src/components/charts/PieChartComponent.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Column from './helper/ColumnList';
 
 const PieChart = () => {
     return (
-        <div>Pie Chart</div>
+        <div>Pie Chart
+                <Column />
+        </div>
     );
 };
 
