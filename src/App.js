fdc02ab43c35cf71fad1028dae04d19d5a6a06b3
Joel Quek <joel.qcx@gmail.com>
2018-09-30 15:11:49 +0900

    added comment

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React from 'react';
 import ChartContainer from './components/ChartContainer';
 import UploadComponent from './components/UploadComponent';
 class App extends React.Component {
-  
+  //initialize state
   state = {
     data:[]
   };
   
+  //load data into state
   updateData = newData => {
     this.setState({data: newData});
   };
 
+
   render(){
     return (
         <div className="App">
+            { /*pass function into uploadcomponent as a callback */  }
             <UploadComponent updateData={this.updateData}/>
+            { /*pass state into chartcontainer */ }
             <ChartContainer data={this.state.data}/>
         </div>
     );
