9eff5df2b9f861e4f63447aa991123dbad2f211a
Joel Quek <joel.qcx@gmail.com>
2018-10-03 22:48:45 +0900

    axis component

--- src/components/charts/BarChartComponent.js
+++ src/components/charts/BarChartComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Column from './helper/Column';
+import Axis from './helper/Axis';
 import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 
@@ -7,6 +8,8 @@ const BarChart = () => {
     return (
         <div>Bar Chart
             <Column />
+
+            <Axis />
         </div>
     );
 };
