883294394fdf4b2865dea9f62bdd6ddd9cfc8c11
Joel Quek <joel.qcx@gmail.com>
2018-10-08 21:43:14 +0900

    initialize columnscontainer

---
+++ src/components/charts/helper/ColumnsContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { DragDropContextProvider } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+import Axis from './Axis';
+import Column from './Column';
+
+class ColumnsContainer extends React.Component{
+    render(){
+        return(
+           <div></div> 
+        );
+    }
+}
+
+export default ColumnsContainer;
